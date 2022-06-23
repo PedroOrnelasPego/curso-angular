3a9c473a322f1be164a51fc5a41e8fed8984908f
unknown <pedroornelaspego@gmail.com>
2022-06-23 14:28:31 -0300

    introdução aos templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: any
+
+  //cursos: string[] = ['Java', 'Ext JS', 'Angular']
+  cursos: string[]
+
+  constructor() {
+
+    this.nomePortal = 'http://pedro.com'
+    this.cursos = ['Java', 'Ext JS','Angular']
+
+   /* for (let i=0; i<this.cursos.length; i++) {
+      let curso =this.cursos[i];
+    } mesma logica apresentada no html*/
+
+  }
 
   ngOnInit(): void {
   }
