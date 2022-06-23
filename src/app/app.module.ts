3ec8f276ad7cfea5e037c9c4e19ef23e87902dbe
unknown <pedroornelaspego@gmail.com>
2022-06-23 10:54:43 -0300

    criação dos primeiros components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule
